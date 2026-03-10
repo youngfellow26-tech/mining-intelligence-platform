aa489526d2b00dde6adc434d642d1ea2489dac3f
youngfellow26-tech <youngfellow26@gmail.com>
2026-03-10 16:32:56 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 @app.get("/")
-def home():
+def root():
     return {"status": "Mining Intelligence Platform Running"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
